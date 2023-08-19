54f1df97408015ee91eb649fbd14e18d2f362d7d
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-19 00:56:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,11 @@ hist_values = np.histogram(
 
 st.bar_chart(hist_values)
 
+st.subheader('Map of all pickups')
+st.map(data)
+
+
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
