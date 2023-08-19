698c729da82df3fd112e8bb62f0e5934f7ab4a15
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-19 06:01:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.title('Uber pickups in NYC')
 st.text('Upload a file')
 uploaded_files = st.file_uploader("Choose an image to upload")
 # If a file is uploaded, display it on the right side.
-if file_upload is not None:
-    image = file_upload.read()
+if uploaded_files is not None:
+    image = uploaded_files.read()
     st.image(image)
 
 DATE_COLUMN = 'date/time'
