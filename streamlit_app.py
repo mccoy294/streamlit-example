b87461c62bbc72777030ef71df2cb2837730c09e
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-19 00:54:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
 
 """
 # Welcome to Streamlit!
@@ -40,7 +41,9 @@ st.write(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    data[DATE_COLUMN].dt.hour, 
+         bins=24, 
+         range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
