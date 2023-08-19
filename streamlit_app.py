259bc975c8d62efd97d6c0cc824d2f1cbf6418bc
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-19 00:50:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,12 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
 
+st.subheader('Raw data')
+st.write(data)
+
+
+
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
