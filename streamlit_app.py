ed685035193ba2ae05583007b69408e8a5228bd7
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-19 00:46:39 -0500

    Update streamlit_app.py
    Grabbing the uber data

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,24 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 st.title('Uber pickups in NYC')
 
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
