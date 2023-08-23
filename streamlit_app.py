d89417fdbd7653b5f9af94e13aa406939d1119dd
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-23 08:27:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
-import streamlit as st
+import easyocr as ocr  #OCR
+import streamlit as st  #Web App
+from PIL import Image #Image Processing
+import numpy as np #Image Processing 
 
-st.title("Hello - Testing")
+#title
+st.title("Easy OCR - Extract Text from Images")
+
+#subtitle
+st.markdown("## Optical Character Recognition - Using `easyocr`, `streamlit`")
+
+st.markdown("")
+
+#image uploader
+image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
