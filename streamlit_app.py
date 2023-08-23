1d23f596c293a659358e8a4c8d2c8e12f62e62de
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-23 08:53:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import easyocr as ocr  #OCR
 import streamlit as st  #Web App
 from PIL import Image #Image Processing
 import numpy as np #Image Processing 
+from google.cloud import vision
+
 
 #title
 st.title("Easy OCR - Extract Text from Images")
@@ -13,6 +15,43 @@ st.markdown("")
 
 #image uploader
 image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+
+st.text("Using GoogleOCR")
+
+# [START vision_text_detection]
+def detect_text(path):
+    """Detects text in the file."""
+
+    client = vision.ImageAnnotatorClient()
+
+    # [START vision_python_migration_text_detection]
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print("Texts:")
+
+    for text in texts:
+        print(f'\n"{text.description}"')
+
+        vertices = [
+            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
+        ]
+
+        print("bounds: {}".format(",".join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+
+
+
+
 
 @st.cache
 def load_model(): 
@@ -28,7 +67,7 @@ if image is not None:
 
     with st.spinner("🤖 AI is at Work! "):
         
-
+        st.text("Using easyOCR")
         result = reader.readtext(np.array(input_image))
 
         result_text = [] #empty list for results
@@ -38,8 +77,14 @@ if image is not None:
             result_text.append(text[1])
 
         st.write(result_text)
+        
+    
     #st.success("Here you go!")
 else:
     st.write("Upload an Image")
+
+st.text("Using GoogleOCR")
+g_output = detect_text(image)
+st.text(g_output)
 
 st.caption("Made by Ryan")
