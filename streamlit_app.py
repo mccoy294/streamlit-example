16735dd3d9af2a20e2a7f7dbb7e34a9cac8ecff3
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-23 21:25:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,14 @@ import easyocr
 #title
 st.title("Easy OCR - Extract Text from Images")
 
-class OCRApp:
-    def __init__(self):
-        st.set_page_config(page_title="OCR App")
-        self.easyocr_reader = easyocr.Reader(lang_list=['en'])
-        self.google_vision_client = vision.ImageAnnotatorClient()
+google_vision_client = vision.ImageAnnotatorClient()
 
-    def run(self):
-        st.title("OCR Comparison App")
-        image = st.file_uploader("Upload an image", type=["jpg", "png"])
+st.title("OCR Comparison APP")
+iamge = st.file_uploader("Upload an image",type = ['jpg','jpeg','png'])
 
-        if image:
-            st.image(image, caption='Uploaded Image.', use_column_width=True)
-            st.write("")
-            if st.button("Process with EasyOCR"):
-                self.process_easyocr(image)
-            if st.button("Process with Google Cloud Vision"):
-                self.process_google_vision(image)
+img = Image.open(image)
 
-    def process_easyocr(self, image):
-        img = Image.open(image)
-        st.write("Running EasyOCR...")
-        result = self.easyocr_reader.readtext(img)
-        for detection in result:
-            text = detection[1]
-            st.write(f"Text: {text}")
-
-    def process_google_vision(self, image):
-        img = Image.open(image)
-        st.write("Running Google Cloud Vision...")
+st.write("Running Google Cloud Vision...")
         content = image.read()
         google_image = vision.Image(content=content)
         response = self.google_vision_client.text_detection(image=google_image)
@@ -42,7 +21,3 @@ class OCRApp:
         for text in texts:
             description = text.description
             st.write(f"Text: {description}")
-
-if __name__ == "__main__":
-    app = OCRApp()
-    app.run()
