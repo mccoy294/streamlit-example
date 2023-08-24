df25024b3a0b8d332decae8d171776a70d9cd3a8
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-23 21:26:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,10 @@ iamge = st.file_uploader("Upload an image",type = ['jpg','jpeg','png'])
 img = Image.open(image)
 
 st.write("Running Google Cloud Vision...")
-        content = image.read()
-        google_image = vision.Image(content=content)
-        response = self.google_vision_client.text_detection(image=google_image)
-        texts = response.text_annotations
-        for text in texts:
-            description = text.description
-            st.write(f"Text: {description}")
+content = image.read()
+google_image = vision.Image(content=content)
+response = self.google_vision_client.text_detection(image=google_image)
+texts = response.text_annotations
+for text in texts:
+        description = text.description
+        st.write(f"Text: {description}")
