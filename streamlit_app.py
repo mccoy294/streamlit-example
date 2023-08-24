82edb0ce60cf1d8a08fa93e5c2e0894fecef110d
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-23 21:35:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ google_vision_client = vision.ImageAnnotatorClient()
 st.title("OCR Comparison APP")
 iamge = st.file_uploader("Upload an image", type=["jpg","jpeg","png"])
 
-img = Image.open(image)
+img = vision.Image(image)
 
 
 
