acf659dab1f090b09d6eb9e923bed4a6c6646203
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-24 20:41:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import cv2
 import PIL
 
 #title
-st.title("Simple Image Processing with OpenCV")
+st.title("Simple Image Processing with OpenCV 1")
 
 # Upload an image
 image = st.file_uploader("Upload an image", type=["jpg", "png","jpeg"])
