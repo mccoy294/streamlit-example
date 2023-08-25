22ed57b98c0f3e4b3d63771204727f5546eb48a3
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-24 20:50:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,9 @@ import cv2
 import PIL
 
 #title
-st.title("Simple Image Processing with OpenCV 2")
+st.title("Simple Image Processing with OpenCV 3")
 
 # Upload an image
 image = st.file_uploader("Upload an image", type=["jpg", "png","jpeg"])
 st.image(image)
 
-# If an image is uploaded and it is a valid image, show it and do some basic processing
-if image and image.type in ["jpg", "png", "jpeg"]:
-    image_string = str(image)
-    img = cv2.imread(image_string)
-    img = Image.fromarray(img)
-    st.image(img)
-
-    # Convert the image to grayscale
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    st.image(gray_img)
-
-    # Apply a blur to the image
-    blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-    st.image(blurred_img)
-
-    # Edge detection
-    edges = cv2.Canny(blurred_img, 50, 150)
-    st.image(edges)
