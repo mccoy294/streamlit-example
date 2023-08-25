a6fbf195544ea28b3c32c5045bc78653fd72cc04
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-24 19:35:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,32 @@ import easyocr
 #title
 st.title("Easy OCR - Extract Text from Images")
 
-google_vision_client = vision.ImageAnnotatorClient()
+class OCRApp:
+    def __init__(self):
+        st.set_page_config(page_title="OCR App")
+        self.google_vision_client = vision.ImageAnnotatorClient()
+    
+    def run(self):
+            st.title("OCR Comparison App")
+            image = st.file_uploader("Upload an image", type=["jpg", "png","jpeg"])
+    
+            if image:
+                st.image(image, caption='Uploaded Image.', use_column_width=True)
+                st.write("")
+                if st.button("Process with Google Cloud Vision"):
+                    self.process_google_vision(image)
 
-st.title("OCR Comparison APP")
-image = st.file_uploader("Upload an image", type=["jpg","jpeg","png"])
+    def process_google_vision(self, image):
+            img = vision.Image(image)
+            st.write("Running Google Cloud Vision...")
+            content = image.read()
+            google_image = vision.Image(content=content)
+            response = self.google_vision_client.text_detection(image=google_image)
+            texts = response.text_annotations
+            for text in texts:
+                description = text.description
+                st.write(f"Text: {description}")
 
-if image is not None:
-    img = vision.Image(image)
+if __name__ == "__main__":
+    app = OCRApp()
+    app.run()
