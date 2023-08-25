31cf37a20d494f42cb45baa2a882513cc8f034db
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 18:42:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ st.title('🎈 App Name')
 
 st.write('Hello world!')
 
+uploaded_file = st.file_uploader("Choose a image file", type = ["jpg","jpeg","png"])
+if uploaded_file is not None:
+    image = uploaded_file.read()
+    img = st.image(image)
