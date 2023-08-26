407a24f1011cedc0eb3a61932b7423f0abc4de45
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 19:01:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import PIL
+import numpy as np
 
 import streamlit as st
 
@@ -8,15 +9,25 @@ st.title('🎈 App Name')
 
 st.write('Hello world!')
 
-uploaded_file = st.file_uploader("Choose a image file", type = ["jpg","jpeg","png"])
-if uploaded_file is not None:
-    image = uploaded_file.read()
-    img = st.image(image)
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Convert the images from BGR to RGB
-    img = cv2.imread(uploaded_file)
+    # Read the uploaded image using PIL
+    image = PIL.Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # Convert PIL image to OpenCV format (numpy array)
+    img_cv = np.array(image)
+
+    # Convert the image from BGR to RGB (OpenCV uses BGR)
+    img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
 
     # Convert the image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    st.image(img)
+    gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+
+    # Display the grayscale image using Streamlit
+    st.image(gray, caption='Grayscale Image', use_column_width=True)
+
+
+
+
