6b1a86790b6e7dedf683b468d61c6516eeee4f1a
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 21:03:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,6 @@ st.title("Text Extract from Bill of Lading")
 # subtitle
 st.markdown("## Verion 1.0.0 of Document reading")
 
-col1, col2 = st.columns(2)
-
-col1.write("This is column 1")
-col2.write("This is column 2")
-
 # upload image file
 file = st.sidebar.file_uploader(label = "Upload your image", type=['png', 'jpg', 'jpeg'])
 
