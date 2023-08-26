4b61444f2188d1c07e04457c5b19dbc3923e4110
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 20:03:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,25 @@ if uploaded_file is not None:
     results = reader.readtext(image)
 
 
+    # loop over the results
+    for (bbox, text, prob) in results:
+	# display the OCR'd text and associated probability
+	print("[INFO] {:.4f}: {}".format(prob, text))
+	# unpack the bounding box
+	(tl, tr, br, bl) = bbox
+	tl = (int(tl[0]), int(tl[1]))
+	tr = (int(tr[0]), int(tr[1]))
+	br = (int(br[0]), int(br[1]))
+	bl = (int(bl[0]), int(bl[1]))
+	# cleanup the text and draw the box surrounding the text along
+	# with the OCR'd text itself
+	text = cleanup_text(text)
+	cv2.rectangle(image, tl, br, (0, 255, 0), 2)
+	cv2.putText(image, text, (tl[0], tl[1] - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+	st.image(image)
+
+
 
 
