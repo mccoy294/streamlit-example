075681495f86ea7602da2ca350fa67eff74cec2c
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 20:33:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,10 @@ st.markdown("## EasyOCRR with Streamlit")
 
 # upload image file
 file = st.file_uploader(label = "Upload your image", type=['png', 'jpg', 'jpeg'])
+
+image = Image.open(file) # read image with PIL library
+st.image(image) #display
+
+# it will only detect the English and Turkish part of the image as text
+reader = easyocr.Reader(['tr','en'], gpu=False) 
+result = reader.readtext(np.array(image))  # turn image to numpy array
