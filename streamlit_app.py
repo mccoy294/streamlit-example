8ec2d0c91a88014ee3707755d4d4eb25c0048a5c
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 20:48:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,37 +12,38 @@ st.title("Text Extract from Bill of Lading")
 # subtitle
 st.markdown("## Verion 1.0.0 of Document reading")
 
-# upload image file
-file = st.file_uploader(label = "Upload your image", type=['png', 'jpg', 'jpeg'])
-
-image = Image.open(file) # read image with PIL library
-st.image(image) #display
-
-# it will only detect the English and Turkish part of the image as text
-reader = easyocr.Reader(['tr','en'], gpu=False) 
-result = reader.readtext(np.array(image))  # turn image to numpy array
-
-textdic_easyocr = {} 
-for idx in range(len(result)): 
-  pred_coor = result[idx][0] 
-  pred_text = result[idx][1] 
-  pred_confidence = result[idx][2] 
-  textdic_easyocr[pred_text] = {} 
-  textdic_easyocr[pred_text]['pred_confidence'] = pred_confidence
-
- # create a dataframe which shows the predicted text and prediction confidence
-df = pd.DataFrame.from_dict(textdic_easyocr).T
-st.table(df)
-
-
-def rectangle(image, result):
-    """ draw rectangles on image based on predicted coordinates"""
-    draw = ImageDraw.Draw(image)
-    for res in result:
-        top_left = tuple(res[0][0]) # top left coordinates as tuple
-        bottom_right = tuple(res[0][2]) # bottom right coordinates as tuple
-        draw.rectangle((top_left, bottom_right), outline="blue", width=2)
-    #display image on streamlit
-    st.image(image)
-
-rectangle(image, result)
+if image_file is not None:
+  # upload image file
+  file = st.file_uploader(label = "Upload your image", type=['png', 'jpg', 'jpeg'])
+  
+  image = Image.open(file) # read image with PIL library
+  st.image(image) #display
+  
+  # it will only detect the English and Turkish part of the image as text
+  reader = easyocr.Reader(['tr','en'], gpu=False) 
+  result = reader.readtext(np.array(image))  # turn image to numpy array
+  
+  textdic_easyocr = {} 
+  for idx in range(len(result)): 
+    pred_coor = result[idx][0] 
+    pred_text = result[idx][1] 
+    pred_confidence = result[idx][2] 
+    textdic_easyocr[pred_text] = {} 
+    textdic_easyocr[pred_text]['pred_confidence'] = pred_confidence
+  
+   # create a dataframe which shows the predicted text and prediction confidence
+  df = pd.DataFrame.from_dict(textdic_easyocr).T
+  st.table(df)
+  
+  
+  def rectangle(image, result):
+      """ draw rectangles on image based on predicted coordinates"""
+      draw = ImageDraw.Draw(image)
+      for res in result:
+          top_left = tuple(res[0][0]) # top left coordinates as tuple
+          bottom_right = tuple(res[0][2]) # bottom right coordinates as tuple
+          draw.rectangle((top_left, bottom_right), outline="blue", width=2)
+      #display image on streamlit
+      st.image(image)
+  
+  rectangle(image, result)
